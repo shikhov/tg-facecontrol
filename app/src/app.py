6dff9c29c852a7e42b445591c5b27f276cfa8fe6
Aleksey Shikhov <shikhov@gmail.com>
2024-12-25 14:59:23 +0500

    refactor Group class to improve initialization and callback handling

--- app/src/app.py
+++ app/src/app.py
@@ -5,7 +5,7 @@ import regex
 
 from aiogram import Bot, Dispatcher, Router, F, types
 from aiogram.utils.text_decorations import html_decoration as hd
-from aiogram.utils.keyboard import InlineKeyboardBuilder as KeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder as KBuilder
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 from aiogram.client.default import DefaultBotProperties
 from dynaconf import ValidationError
@@ -13,11 +13,14 @@ from config import config
 
 
 class Group:
-    def __init__(self, chat_id=None, chat=None):
-        if chat_id:
-            data = config.groups[int(chat_id)]
-        elif chat:
-            data = config.groups[chat.id]
+    def __init__(self, message: types.Message = None, request: types.ChatJoinRequest = None, callback: types.CallbackQuery = None):
+        if message:
+            data = config.groups[message.chat.id]
+        elif request:
+            data = config.groups[request.chat.id]
+        elif callback:
+            (self.answer, self.chat_id, self.chat_username) = callback.data.split('#')
+            data = config.groups[int(self.chat_id)]
         else:
             raise('Error object initialization')
 
@@ -31,31 +34,80 @@ class Group:
         self.captcha_timeout = data.get('captcha_timeout', config.defaults.captcha_timeout)
         self.delete_joins = data.get('delete_joins', config.defaults.delete_joins)
         self.logchatid = data.get('logchatid', config.defaults.logchatid)
+        self.user = None
 
-        if chat:
-            self.chat = chat
-            self.welcome_text = self.welcome_text.replace('%CHAT_TITLE%', chat.title)
+        if message:
+            self.user = message.from_user
 
-    def is_right_answer(self, answer):
-        return answer == self.emoji_list[0]
+        if request:
+            self.chat = request.chat
+            self.user = request.from_user
+            self.welcome_text = self.welcome_text.replace('%CHAT_TITLE%', self.chat.title)
+
+        if callback:
+            self.user = callback.from_user
+            self.msg_id = callback.message.message_id
+
+        self.loguser = f'{self.user.full_name} @{self.user.username}' if self.user.username else self.user.full_name
+        self.loguser = hd.quote(self.loguser)
+
+    def is_right_answer(self):
+        return self.answer == self.emoji_list[0]
 
     def buttons(self):
-        kb = KeyboardBuilder()
+        kb = KBuilder()
         for emoji in random.sample(self.emoji_list, len(self.emoji_list)):
             kb.button(text=emoji, callback_data=f"{emoji}#{self.chat.id}#{self.chat.username or ''}")
         kb.adjust(self.emoji_rowsize)
         return kb.as_markup()
 
-    async def send_captcha(self, user):
-        logname = hd.quote(f'{user.full_name} (@{user.username})' if user.username else user.full_name)
-        message = await bot.send_message(user.id, self.welcome_text, reply_markup=self.buttons())
-        await log(self.logchatid, f'{logname} wants to join {self.chat.title}')
+    def chat_link_button(self):
+        if not self.chat_username:
+            return None
+        kb = KBuilder().button(text='Перейти', url=f'https://t.me/{self.chat_username}')
+        return kb.as_markup()
+
+    async def send_captcha(self):
+        message = await bot.send_message(self.user.id, self.welcome_text, reply_markup=self.buttons())
+        await self.log(f'{self.loguser} is trying to join {self.chat.title}')
         await asyncio.sleep(self.captcha_timeout)
         try:
-            await bot.decline_chat_join_request(self.chat.id, user.id)
+            await bot.decline_chat_join_request(self.chat.id, self.user.id)
         except Exception:
             return
         await message.edit_text(self.timeout_text)
+
+    async def handle_callback(self):
+        if self.is_right_answer():
+            try:
+                await bot.approve_chat_join_request(self.chat_id, self.user.id)
+            except Exception:
+                await bot.edit_message_text(self.error_text, chat_id=self.user.id, message_id=self.msg_id)
+                return
+
+            await bot.edit_message_text(
+                text=self.success_text,
+                chat_id=self.user.id,
+                message_id=self.msg_id,
+                reply_markup=self.chat_link_button()
+            )
+            await self.log(f'{self.loguser} succeeded')
+        else:
+            await bot.edit_message_text(self.fail_text, chat_id=self.user.id, message_id=self.msg_id)
+            try:
+                await bot.decline_chat_join_request(self.chat_id, self.user.id)
+            except Exception:
+                return
+            await self.log(f'{self.loguser} failed')
+
+    async def log(self, text):
+        logging.info(text)
+        if not self.logchatid:
+            return
+        try:
+            await bot.send_message(self.logchatid, text)
+        except Exception:
+            logging.warning(f'Cannot log to chat id: {self.logchatid}')
 
 
 # Configure logging
@@ -77,15 +129,6 @@ config.groups = {group['id']: group for group in config.groups}
 bot = Bot(token=config.bot_token, default=DefaultBotProperties(parse_mode='HTML'))
 dp = Dispatcher()
 router = Router()
-
-
-async def log(logchatid, text):
-    logging.info(text)
-    if logchatid:
-        try:
-            await bot.send_message(logchatid, text)
-        except Exception:
-            logging.warning(f'Cannot log to chat id: {logchatid}')
 
 
 async def isChatAllowed(chat: types.Chat):
@@ -115,7 +158,7 @@ async def outer_middleware(handler, event, data):
 async def deleteJoinMessage(message: types.Message):
     if message.chat.id not in config.groups:
         return
-    group = Group(chat=message.chat)
+    group = Group(message=message)
     if not group.delete_joins:
         return
 
@@ -126,39 +169,16 @@ async def deleteJoinMessage(message: types.Message):
 
 
 @router.chat_join_request()
-async def processJoinRequest(update: types.ChatJoinRequest):
-    if update.chat.id not in config.groups:
-        logging.warning(f'ChatJoinRequest from unknown group: {update.chat.id} {update.chat.title}')
+async def processJoinRequest(request: types.ChatJoinRequest):
+    if request.chat.id not in config.groups:
+        logging.warning(f'ChatJoinRequest from unknown group: {request.chat.id} {request.chat.title}')
         return
-    await Group(chat=update.chat).send_captcha(update.from_user)
+    await Group(request=request).send_captcha()
 
 
 @router.callback_query()
-async def callbackHandler(query: types.CallbackQuery):
-    user = query.from_user
-    msg_id = query.message.message_id
-    logname = hd.quote(f'{user.full_name} (@{user.username})' if user.username else user.full_name)
-    (answer, chat_id, chat_username) = query.data.split('#')
-    group = Group(chat_id=chat_id)
-    if group.is_right_answer(answer):
-        try:
-            await bot.approve_chat_join_request(chat_id, user.id)
-        except Exception:
-            await bot.edit_message_text(group.error_text, chat_id=user.id, message_id=msg_id)
-            return
-
-        kb = None
-        if chat_username:
-            kb = KeyboardBuilder().button(text='Перейти', url='https://t.me/' + chat_username).as_markup()
-        await bot.edit_message_text(group.success_text, chat_id=user.id, message_id=msg_id, reply_markup=kb)
-        await log(group.logchatid, f'{logname} succeeded')
-    else:
-        await bot.edit_message_text(group.fail_text, chat_id=user.id, message_id=msg_id)
-        try:
-            await bot.decline_chat_join_request(chat_id, user.id)
-        except Exception:
-            return
-        await log(group.logchatid, f'{logname} failed')
+async def callbackHandler(callback: types.CallbackQuery):
+    await Group(callback=callback).handle_callback()
 
 
 async def main():
